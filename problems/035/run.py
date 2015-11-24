fecc8bf8420eae122b48efd186d407afb42fd503
Brian Chan <bchan1@fb.com>
2015-11-24 09:57:04 -0800

    improved runtime from 6 sec to 1.5 sec

--- problems/035/run.py
+++ problems/035/run.py
@@ -1,10 +1,10 @@
 
 import math
 
-def create_a_bucket_with_primes(n):
+def create_a_bucket_with_primes_below(n):
   bucket = {}
   for i in range(2, n+1, 1):
-    bucket[i] = 1
+    bucket[i] = 0  # 0 means untouched/unvisited
   up_to = int(math.ceil(math.sqrt(n)))
   for multiplier in range(2, up_to, 1):
     if not bucket.has_key(multiplier):  # multiples of 2 took care of multiples of 4
@@ -21,19 +21,32 @@ def get_rotations(n):
   rotations = []
   s = str(n)
   for i in range(len(s)):
-    rotations.append(s[i:len(s)] + s[0:i])
+    rotation = s[i:len(s)] + s[0:i]
+    rotations.append(int(rotation))
   return rotations
 
 def all_rotations_are_prime(rotations, primes_bucket):
+  all_primes = True
   for rotation in rotations:
-    if not int(rotation) in primes_bucket:
-      return False
-  return True
+    if not rotation in primes_bucket:
+      all_primes = False
+      break
+  if all_primes:
+    fill_markers(rotations, primes_bucket, 1)
+  return all_primes
+
+def fill_markers(rotations, primes_bucket, val):
+  # 0 is unvisited, 1 means circular prime
+  for rotation in rotations:
+    primes_bucket[rotation] = val
 
 def count_circular_primes_below(n):
-  prime_bucket = create_a_bucket_with_primes(n)
+  prime_bucket = create_a_bucket_with_primes_below(n)
   count = 0
-  for i in range(2, n+1, 1):
+  for i in prime_bucket.keys():
+    if prime_bucket[i] == 1:  # already calculated
+      count += 1
+      continue
     rotations = get_rotations(i)
     if all_rotations_are_prime(rotations, prime_bucket):
       count += 1
