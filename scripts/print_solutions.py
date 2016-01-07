63d2ef6a5ac3f2c95369a3de3c6a1bc89757502b
Brian Chan <bchan1@fb.com>
2016-01-07 14:39:55 -0800

    need to change current dir for run.py that takes input files

--- scripts/print_solutions.py
+++ scripts/print_solutions.py
@@ -7,9 +7,10 @@ path = './problems/'
 def print_one_solution(path, dirname):
   start_time = time.time()
   print "// --------------------------------"
-  cmd = "python " + path + dirname + '/' + 'run.py'
-  print cmd
-  os.system(cmd)
+  os.chdir(path + dirname)
+  print "python " + path + dirname + '/' + 'run.py'
+  os.system('python run.py')
+  os.chdir('../../')
   end_time = time.time()
   time_delta = "%.3f" % (end_time-start_time)
   print "Time spent: " + str(time_delta) + " seconds"
